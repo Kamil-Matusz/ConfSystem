2a8cf0aa4470cb9bca4c36b3a0f1c4a7dfa1fbe9
Kamil Matusz <matuszkamil076@gmail.com>
2023-10-21 19:22:56 +0200

    Created Attendances entities & migrations

--- src/Modules/Attendances/ConfSystem.Modules.Attendances.Infrastructure/Extensions.cs
+++ src/Modules/Attendances/ConfSystem.Modules.Attendances.Infrastructure/Extensions.cs
@@ -1,3 +1,5 @@
+using ConfSystem.Modules.Attendances.Infrastructure.DAL;
+using ConfSystem.Shared.Infrastructure.PostgreSQL;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ConfSystem.Modules.Attendances.Infrastructure;
@@ -6,6 +8,7 @@ public static class Extensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        services.AddPostgres<AttendancesDbContext>();
         return services;
     } 
 }
