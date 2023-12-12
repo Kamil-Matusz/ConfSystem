25c1e07c0e70b0868a6eb144b08bf56bc6078084
Kamil Matusz <matuszkamil076@gmail.com>
2023-12-12 19:34:26 +0100

    Adding JWT support to the swagger documentation

--- src/Shared/ConfSystem.Shared.Infrastructure/Extensions.cs
+++ src/Shared/ConfSystem.Shared.Infrastructure/Extensions.cs
@@ -73,6 +73,30 @@ internal static class Extensions
                 Title = "ConfSystem",
                 Version = "v1"
             });
+            
+            swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below.",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+            
+            swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
         });
         
         // Module List generator
